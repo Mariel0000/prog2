4051b3e7171f15d57e60bb7621ab0de894545b10
Mariel0000 <marielmodric@gmail.com>
2025-05-29 13:20:08 -0400

    cambio 1

--- TRABAJO-LAB-DE-PROGRAMACION-II.cpp
+++ TRABAJO-LAB-DE-PROGRAMACION-II.cpp
@@ -2,12 +2,26 @@
 #include<fstream>
 #include<string>
 #include<conio.h>
+#include<cctype>
+#include<vector>
 using namespace std;
 
 int main () {
 	
 	cout<<"Prueba git bashP"<<endl;
 	cout<<"prueba 2"<<endl;
+	 
+	String nombreArchivo;
+	String palabraBuscar;
+	Char repeat = ‘s’;
+	While (repetir == ‘s’ || repetir == ‘S’) {
+		cout <<"ingrese el nombre del archivo"<<nombreArchivo<<endl;
+		Ifstream archivo nombreArchivo);
+		if (¡archivo.is_open) {
+		Cerr << “Error al abrir el archivo.” << endl;
+		Return 1;
+ }
+	
 	
 	getch();
 	return 0;
