b0f0dca13ac5432e123d6244f054ba0103dc4162
Mariel0000 <marielmodric@gmail.com>
2025-05-29 22:05:59 -0400

    prueba 3

--- TRABAJO-LAB-DE-PROGRAMACION-II.cpp
+++ TRABAJO-LAB-DE-PROGRAMACION-II.cpp
@@ -1,28 +1,30 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<conio.h>
-#include<cctype>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <vector>
+
 using namespace std;
 
-int main () {
+void setColor(int color) {
+    cout << "\033[" << color << "m";
+}
+
+int main() {
+    string nombreArchivo;
+    string palabraBuscar;
+    char repetir = 's';
+
+    while (repetir == 's' || repetir == 'S') {
+        cout << "Ingrese el nombre del archivo: ";
+        cin >> nombreArchivo;
+
+        ifstream archivo(nombreArchivo);
+        if (!archivo.is_open()) {
+            cerr << "Error al abrir el archivo." << endl;
+            return 1;
+        }
+	}
 	
-	cout<<"Prueba git bashP"<<endl;
-	cout<<"prueba 2"<<endl;
-	 
-	String nombreArchivo;
-	String palabraBuscar;
-	Char repeat = ‘s’;
-	While (repetir == ‘s’ || repetir == ‘S’) {
-		cout <<"ingrese el nombre del archivo"<<nombreArchivo<<endl;
-		Ifstream archivo nombreArchivo);
-		if (¡archivo.is_open) {
-		Cerr << “Error al abrir el archivo.” << endl;
-		Return 1;
- }
-	
-	
-	getch();
 	return 0;
 }
