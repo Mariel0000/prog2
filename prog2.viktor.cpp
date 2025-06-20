e9d05384680a8cba9d5696dcd5e6ee4ca0b2e2f4
Mariel0000 <marielmodric@gmail.com>
2025-06-19 23:23:27 -0400

    Update prog2.viktor.cpp

--- prog2.viktor.cpp
+++ prog2.viktor.cpp
@@ -16,6 +16,68 @@ struct Producto {
 Producto* listaProductos = nullptr;
 int cantidadProductos = 0;
 
+void agregarProducto() {
+    Producto* nuevoArreglo = new Producto[cantidadProductos + 1];
+
+    for (int i = 0; i < cantidadProductos; i++) {
+        nuevoArreglo[i] = listaProductos[i];
+    }
+
+    cout << "Ingrese el código: ";
+    cin >> nuevoArreglo[cantidadProductos].codigo;
+    cin.ignore();
+    cout << "Ingrese el nombre: ";
+    getline(cin, nuevoArreglo[cantidadProductos].nombre);
+    cout << "Ingrese el precio: ";
+    cin >> nuevoArreglo[cantidadProductos].precio;
+    cout << "Ingrese el stock: ";
+    cin >> nuevoArreglo[cantidadProductos].stock;
+    cin.ignore();
+    cout << "Ingrese la categoría: ";
+    getline(cin, nuevoArreglo[cantidadProductos].categoria);
+    nuevoArreglo[cantidadProductos].activo = true;
+
+    delete[] listaProductos;
+    listaProductos = nuevoArreglo;
+    cantidadProductos++;
+
+    cout << "Producto agregado." << endl;
+}
+
+void eliminarProducto() {
+    int codigoEliminar;
+    cout << "Ingrese el código del producto a eliminar: ";
+    cin >> codigoEliminar;
+
+    int posicion = -1;
+    for (int i = 0; i < cantidadProductos; i++) {
+        if (listaProductos[i].codigo == codigoEliminar) {
+            posicion = i;
+            break;
+        }
+    }
+
+    if (posicion == -1) {
+        cout << "Producto no encontrado." << endl;
+        return;
+    }
+
+    Producto* nuevoArreglo = new Producto[cantidadProductos - 1];
+
+    int indiceNuevo = 0;
+    for (int i = 0; i < cantidadProductos; i++) {
+        if (i != posicion) {
+            nuevoArreglo[indiceNuevo++] = listaProductos[i];
+        }
+    }
+
+    delete[] listaProductos;
+    listaProductos = nuevoArreglo;
+    cantidadProductos--;
+
+    cout << "Producto eliminado." << endl;
+}
+
 
 int main() {
     string nombreArchivo = "Inventario.bin";
